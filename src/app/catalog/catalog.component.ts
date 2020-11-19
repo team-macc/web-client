6b5a86cccfdaf04268e27735ea3fdfedd774222a
Samuel Souza <samuelsouza.contact@gmail.com>
2020-11-19 14:21:36 -0300

    creating list of products

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PRODUCTS } from './mock-products';
+
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss']
 })
 export class CatalogComponent implements OnInit {
-
+  listOfProducts = PRODUCTS;
+  
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/catalog/mock-products.ts
@@ -0,0 +1,14 @@
+import { Product } from './product';
+
+export const PRODUCTS: Product[] = [
+    {id: 1, name: 'Cadeira Presidente para Escritório Couro', description: 'Acabamento cromado que protege e confere requinte', picture: 'https://images-na.ssl-images-amazon.com/images/I/41DWuJOF0jL._AC_SX184_.jpg'},
+    {id: 2, name: 'Cadeira Gamer Kids em Couro', description: 'Desenvolvida para proporcionar conforto e boa postura, a cadeira foi projetada especialmente para os mais jovens', picture: 'https://images-na.ssl-images-amazon.com/images/I/41rEHLy8Z4L._AC_SX184_.jpg'},
+    {id: 3, name: 'Monitor Professional Full HD IPS 23,8" Widescreen Dell P2419H Preto', description: 'Monitor 23.8” Dell P2419H Professional - Full HD IPS - 5ms - Regulagem de Altura e Rotação - Furação VESA - USB - DisplayPort/HDMI/VGA - Garantia 3 anos', picture: 'https://images-na.ssl-images-amazon.com/images/I/412lHH+s4zL._AC_SX184_.jpg'},
+    {id: 4, name: 'Display Interativo Wacom One, DTC133W0A', description: 'Compatibilidade com diversos sistemas (Mac, PC, Android)', picture: 'https://images-na.ssl-images-amazon.com/images/I/41EbpK1KTVL._AC_SX184_.jpg'},
+    {id: 5, name: 'Razer Viper RZMOVP01RT - Mouse Ambidestro, 16.000 DPI, Sensor Óptico, Preto (Chroma RGB)Razer Viper RZMOVP01RT', description: '', picture: 'https://images-na.ssl-images-amazon.com/images/I/31LsJuF1X-L._AC_SX184_.jpg'},
+    {id: 6, name: 'Razer Headset Kraken X Lite Multi Platform, Razer, Fone de Ouvido com MicrofoneRazer Headset Kraken X', description: '', picture: 'https://images-na.ssl-images-amazon.com/images/I/31eKoMebW5L._AC_SX184_.jpg'},
+    {id: 7, name: 'PLACA DE VIDEO NVIDIA GEFORCE GTX 1660 OC', description: '', picture: 'https://images-na.ssl-images-amazon.com/images/I/51rpKALlyDL._AC_SX184_.jpg'},
+    {id: 8, name: 'Notebook Gamer Dell G5-5590-M73B 15.6" 9ª G Intel Core i7 16GB 512GB SSD Placa Vídeo NVIDIA 1660 Microsof', description: '', picture: 'https://images-na.ssl-images-amazon.com/images/I/4124ppJIZ3L._AC_SX184_.jpg'},
+    {id: 9, name: 'Teclado Gamer Mecâmico Shooter LED 7 Cores Cabo 1,5 ELG TGMS', description: '', picture: 'https://images-na.ssl-images-amazon.com/images/I/41bLHam8dAL._AC_SX184_.jpg'},
+    {id: 10, name: 'Notebook Ultrafino Dell Inspiron 5590-M30S 10ª Ger. Intel Core i7 16GB 256GB SSD NVIDIA Full HD 15.6" WinNotebook Ultrafino Dell', description: '', picture: 'https://images-na.ssl-images-amazon.com/images/I/41Ls5eTJu6L._AC_SX184_.jpg'}
+];
