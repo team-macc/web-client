5884e2167b2910ba10294018067b90c16ad1d38b
Samuel Souza <samuelsouza.contact@gmail.com>
2020-12-14 15:53:06 -0300

    auth service

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {auth, signInWithGoogle} from '../../firebase/firebase.utils.js';
+import { loginWithEmailAndPassword } from '../../services/auth.service';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -21,7 +21,7 @@ class SignIn extends React.Component{
         const {email, password} = this.state;
 
         try{
-          await auth.signInWithEmailAndPassword(email, password);
+          await loginWithEmailAndPassword(email, password);
           this.setState({ email: "", password: "" });
         }catch(error){
           console.log(error)
